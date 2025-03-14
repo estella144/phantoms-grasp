cab03adb11c9a69d1bd9290eb7ca855bfd63881c
Oliver <oliverng2512@icloud.com>
2025-03-14 11:23:27 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 
-VERSION = "0.2.0.dev3"
+INVENTORY_LIMIT = 2
+VERSION = "0.2.0.dev4"
 
 class Item:
     """
@@ -72,6 +73,34 @@ class Room:
         self.is_locked = is_locked
         self.exits = exits
 
+def check_key(room_number: int, items: list[Item]):
+    for item in items:
+        if (isinstance(item, Key) and item["location"] == 999):
+            if room_number in item["unlocks"]:
+                return True
+    return False
+
+def get_new_room_number(map_length: int) -> int:
+    """
+    Requests a room number (map index) and validates it.
+    Returns the desired room number if it is valid, and -1 otherwise.
+    """
+
+    try:
+        new_room_number = int(input("Go to a room number: "))
+        if new_room_number < 0:
+            print("That's not a positive whole number!")
+            return -1
+    except ValueError:
+        print("That's not a positive whole number!")
+        return -1
+    
+    if new_room_number >= map_length:
+        print("There is no room with that number!")
+        return -1
+    
+    return new_room_number
+    
 def create_item(location: int, item_data: dict) -> Item:
     if item_data["type"] == "Item":
         return Item(
@@ -121,7 +150,7 @@ def get_map_data(mapfile: str) -> tuple:
         print("Error: Incompatible map data.")
         input("Press [Enter] to quit")
         quit()
-    
+
     for room_data in data['room_data']:
         room = Room(
             number=room_data['number'],
@@ -133,8 +162,10 @@ def get_map_data(mapfile: str) -> tuple:
         )
         rooms.append(room)
 
+        print(room_data.get("items", []))
         for item_data in room_data.get("items", []):
             item = create_item(room_data['number'], item_data)
+            items.append(item)
     
     return rooms, items, metadata
 
@@ -146,13 +177,30 @@ def main() -> None:
     leave = False
     current_room = metadata['spawn_room']
     entrance_room = metadata['entrance_room']
+    items_held = 0
     
     while not leave:
-        if game_map[current_room].is_lit == False:
+
+        print(items)
+        player_light = False
+        for item in items:
+            if (item.provides_light == True
+                and item.location == 999):
+                player_light = True
+        
+        if (game_map[current_room].is_lit == False
+            and player_light == False):
             print("This is a dark room, you can't see anything.")
         else:
             print(game_map[current_room].name)
             print(game_map[current_room].description)
+            for item in items:
+                if item.location == current_room:
+                    print(f"There is a {item.name} in the room.")
+                    pick_up = input("Pick up the item? [Y/N]").lower()
+                    if pick_up == "y":
+                        items[items.index(item)].location = 999
+                        items_held += 1
 
         if current_room == 0:
             choice = input("You have reached the entrance.",
@@ -162,25 +210,15 @@ def main() -> None:
                 leave = True
                 break
         
-        try:
-            new_room_number = int(input("Go to a room number: "))
-            if new_room_number < 0:
-                print("That's not a positive whole number!")
+        new_room_number = get_new_room_number(len(game_map))
+        
+        if new_room_number != -1:
+            # -1 indicates an invalid input
+            if (game_map[new_room_number].is_locked == True
+                and check_key(new_room_number, items) == False):
+                print("This room is locked. Find a key to unlock it.")
                 continue
-        except ValueError:
-            print("That's not a positive whole number!")
-            continue
-        
-        try:
-            new_room = game_map[new_room_number]
-        except IndexError:
-            print("There is no room with that number!")
-            continue
-        
-        if new_room.is_locked == True:
-            print("This room is locked. You can't go in there!")
-            continue
-        current_room = new_room_number
+            current_room = new_room_number
 
     print("The door opens, revealing the night sky outside.")
     print("You have escaped.")
